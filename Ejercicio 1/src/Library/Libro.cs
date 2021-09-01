e136ef856ee4103d97a3b5116e035c0b7ca33508
Santiago Ferraro <santiago.ferraro@correo.ucu.edu.uy>
2021-09-01 09:56:09 -0300

    Comentarios para el ejercicio 1

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -4,7 +4,10 @@ namespace SRP
 {
     public class Libro
     {
-
+        /* 
+        La clase Libro tiene demasiadas resposabilidades, deberia crearse una nueva clase biblioteca
+        que se encargue de ver el sector y estante en que se deba almacenar el libro.
+        */
         public string Titulo { get ; }
         public string Autor { get ; }
         public string Codigo { get ;  }
@@ -17,7 +20,10 @@ namespace SRP
             this.Autor = autor;
             this.Codigo = codigo;
         }
-
+        /* 
+            Este metodo perteneceria a dicha nueva clase "biblioteca", la clase Libro es experta en crear libros, tiene la responsabilidad de
+            conocer los datos necesarios para crear los objetos de tipo libro.
+        */
         public void AlmacenarLibro(String sector, String estante)
         {
             this.SectorBiblioteca = sector;
